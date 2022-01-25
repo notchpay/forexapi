fe073967db688e550f94645b422cbaf8a8720f15
chapdel <drop@chapdel.me>
2022-01-25 10:00:26 +0100

    Added currency calculator

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,9 +15,11 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
     <livewire:styles />
     <livewire:scripts />
+    <wireui:scripts />
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
 </head>
 
 <body class="font-sans antialiased">
